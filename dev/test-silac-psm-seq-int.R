57c284e79040f28c6d37f465972c9f51455635cc
Charlotte Dawson <csdaw@outlook.com>
2022-11-03 16:56:53 +0000

    Try to understand PSM to peptide

---
+++ dev/test-silac-psm-seq-int.R
@@ -0,0 +1,42 @@
+library(camprotR)
+
+psms <- read.delim("dev/data-raw/RNA_IP_rand_PSMs.txt") %>% 
+  mutate(id = paste(Sequence, Modifications, sep = ":"))
+
+pepg <- read.delim("dev/data-raw/RNA_IP_rand_PeptideGroups.txt") %>% 
+  mutate(id = paste(Sequence, Modifications, sep = ":"))
+
+psms_tally <- psms %>% 
+  group_by(across(c(Sequence, Modifications))) %>% 
+  tally()
+
+all_pepg_seqs <- psms_tally$Sequence
+
+pepg_seqs <- pepg$Sequence %>% sort()
+
+# check number of peptide sequences
+nrow(pepg)
+nrow(psms_tally)
+
+# pepg seqs are a subset of psms
+# but some things in our grouped psms do not make it to pepg
+all(pepg_seqs %in% all_pepg_seqs)
+
+# which are they?
+missing_pepg <- psms_tally[which(!all_pepg_seqs %in% pepg_seqs), ]
+missing_psms <- psms[which(psms$Sequence %in% missing_pepg$Sequence), ]
+
+non_missing_psms <- psms[which(!psms$PSMs.Peptide.ID %in% missing_psms$PSMs.Peptide.ID), ]
+non_missing_psms2 <- psms[which(psms$Sequence %in% pepg$Sequence), ]
+
+# missing and non-missing psms should add up to psms
+nrow(missing_psms) + nrow(non_missing_psms) == nrow(psms)
+nrow(missing_psms) + nrow(non_missing_psms2) == nrow(psms)
+
+
+
+psms_of_interest <- psms %>% 
+  filter(Sequence == "TNIIPVLEDAR" | Sequence == "VFIGNLNTAIVK")
+
+
+
